01f7a4cac79ea8cb3bdb96f97be77a965f079e6f
buchanaf <buchanaf@gmail.com>
2014-06-05 20:16:18 -0400

    Start and Stop functionality on search term

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,23 +5,20 @@ angular.module('socketIoTwitterApp')
     var socket = io();
     $scope.tweets = [];
 
-
     socket.on('tweet', function(data){
-      if(data  && data.user.name && data.user.location){
-        $scope.tweets.push(data);
+      if(data && data.user.name && data.user.location){
+        $scope.tweets.unshift(data);
         $scope.$apply();
       }
     })
-
-
 
     $scope.getTweets = function (phrase) {
       $scope.phrase = phrase;
       socket.emit('tweet-start', $scope.phrase);
     };
 
-    $scope.endTweets = function (phrase) {
-      socket.emit('tweet-end');
+    $scope.endTweets = function () {
+      socket.emit('tweet-end')
     };
 
   });
