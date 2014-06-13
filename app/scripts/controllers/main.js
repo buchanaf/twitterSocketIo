2142d903febc7c68664273b7bbfaffc6acdcfe2c
buchanaf <buchanaf@gmail.com>
2014-06-13 16:29:22 -0400

    updated stying and button

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,11 +9,12 @@ angular.module('socketIoTwitterApp')
     $scope.success = false;
     $scope.retweetResult = "Tweet Success";
     $scope.phrase;
-    console.log($scope.phrase)
+
+
 
     socket.emit('tweet-end');
-
     Session.get({}, function(data){
+      $scope.user = data;
       $rootScope.currentUser = data;
       $http({
         method: 'GET',
@@ -21,13 +22,14 @@ angular.module('socketIoTwitterApp')
         params: {user_name: $rootScope.currentUser.name}
       })
       .success(function(data){
+        console.log(data)
         $scope.followers = data;
       });
     })
 
     socket.on('tweet', function(data){
       if(data.tweeter && data.tweeter.user.name && data.tweeter.user.location){
-        if($scope.tweets.length>=3){
+        if($scope.tweets.length>=4){
           return
         }
 
@@ -48,15 +50,17 @@ angular.module('socketIoTwitterApp')
 
     $scope.getTweets = function (phrase) {
       socket.emit('tweet-start', phrase);
+      console.log($rootScope.currentUser)
       $scope.streaming = false;
       $scope.phrase = "";
-      $scope.currentTerm = phrase
+      $scope.currentTerm = phrase;
       $scope.$apply();
     };
 
     $scope.endTweets = function () {
       socket.emit('tweet-end')
       $scope.streaming = true;
+      $scope.searchterm.searchphrase.$setPristine();
       $scope.$apply();
     };
 
