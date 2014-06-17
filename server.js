c05de24173ed3a171b3f3cdbae6e8c0dbb909a8c
buchanaf <buchanaf@gmail.com>
2014-06-17 10:42:16 -0400

    refactor sockets.js

--- server.js
+++ server.js
@@ -50,52 +50,6 @@ var socketConnect = new socket(server);
 // // Connect Socket
 
 
-//configure passport for twitter
-// passport.use(new TwitterStrategy({
-//       consumerKey: 'Gxeq7uwK24Wwge8dWP1AeUsMP',
-//       consumerSecret: 'IcnCGchu3xT6RQPxpz2WJdIqeSiFVA98WOQIsVQzcGm9GAgbYM',
-//       callbackURL: 'http://127.0.0.1:9000/auth/twitter/callback'
-//   },
-//   function(token, tokenSecret, profile, done) {
-//      process.nextTick(function() {
-
-//         // find the user in the database based on their facebook id
-//         User.findOne({ 'twitter.id' : profile.id }, function(err, user) {
-
-//             // if there is an error, stop everything and return that
-//             // ie an error connecting to the database
-//             if (err)
-//                 return done(err);
-//             // if the user is found, then log them in
-//             if (user) {
-//                 return done(null, user); // user found, return that user
-//             } else {
-//                 // if there is no user found with that facebook id, create them
-//                 var newUser = new User();
-
-//                 // set all of the facebook information in our user model
-//                 newUser.twitter.id    = profile.id; // set the users facebook id
-//                 newUser.twitter.token = token; // we will save the token that facebook provides to the user
-//                 newUser.twitter.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
-//                 newUser.twitter.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
-
-//                 // save our user to the database
-//                 newUser.save(function(err) {
-//                     if (err)
-//                         throw err;
-
-//                     // if successful, return the new user
-//                     return done(null, newUser);
-//                 });
-//             }
-
-//         });
-//     });
-//   }
-// ));
-// twitterlogin();
-
-
 
 // Expose app
 exports = module.exports = app;
